f4500fbef61fb3965238072478ae4d6e5587ba84
Caio Victor Kodato Teixeira <caiovictorkodatot@gmail.com>
2024-02-10 17:57:39 -0300

    Curso atual

--- src/entitiesRevi/Product.java
+++ src/entitiesRevi/Product.java
@@ -5,6 +5,7 @@ public class Product {
     private double price;
     private int quantity;
 
+
     public Product() {
 
     }
@@ -14,18 +15,17 @@ public class Product {
         this.quantity = quantity;
     }
 
-
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public double getPrice() {
@@ -34,6 +34,10 @@ public class Product {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public double totalValueInStock() {
@@ -47,6 +51,8 @@ public class Product {
     public void removeProducts(int quantity) {
             this.quantity -= quantity;
     }
+
+
 
     public String toString() {
         return name + ", $" + String.format("%.2f",price) + ", " + quantity +  " units, Total: $" + String.format("%.2f",totalValueInStock());
