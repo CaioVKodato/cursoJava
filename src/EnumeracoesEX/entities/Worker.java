350cb4465481e8247ce6b40b44b2bddb74c7b57f
Caio Victor Kodato Teixeira <caiovictorkodatot@gmail.com>
2024-03-05 21:30:40 -0300

    Continuação do exercicios - Foi implementada alguns metodos necessarios

--- src/EnumeracoesEX/entities/Worker.java
+++ src/EnumeracoesEX/entities/Worker.java
@@ -3,6 +3,7 @@ package EnumeracoesEX.entities;
 import EnumeracoesEX.entitiesEnum.WorkerLevel;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Worker {
@@ -22,5 +23,65 @@ public class Worker {
         this.level = level;
         this.baseSalary = baseSalary;
         this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContracts (HourContract contract) {
+        contracts.add(contract);
+    }
+
+    public void removeContracts (HourContract contract) {
+        contracts.remove(contract);
+    }
+
+    public double income(int year, int month) {
+        double soma = baseSalary;
+        Calendar cal = Calendar.getInstance();
+
+        for (HourContract c : contracts) {
+            cal.setTime(c.getDate());
+            int c_year = cal.get(Calendar.YEAR);
+            int c_month = 1 + cal.get(Calendar.MONTH);
+                if (year == c_year && month == c_month) {
+                    soma += c.totalValue();
+            }
+        }
+
+        return soma;
     }
 }
