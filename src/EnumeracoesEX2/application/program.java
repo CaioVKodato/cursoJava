2b40a976ca3b918d955240f810adfa0477e11372
Caio Victor Kodato Teixeira <caiovictorkodatot@gmail.com>
2024-03-07 22:54:26 -0300

    Segundo post

--- src/EnumeracoesEX2/application/program.java
+++ src/EnumeracoesEX2/application/program.java
@@ -14,11 +14,25 @@ public class program {
         Comment c1 = new Comment("Have a nice trip !");
         Comment c2 = new Comment("Wow that's awesome");
         Post p1;
-        p1 = new Post(sdf.parse("21/06/2022 13:05:44"),"Traveling to New Zeland",
-                "I'm going to visit this wonderful country",12);
+        p1 = new Post(sdf.parse("21/06/2022 13:05:44"), "Traveling to New Zeland",
+                "I'm going to visit this wonderful country", 12);
         p1.addComment(c1);
         p1.addComment(c2);
 
         System.out.println(p1);
+
+        System.out.println("---------------------------------------------------------------------");
+
+        Comment c3 = new Comment("Good night");
+        Comment c4 = new Comment("May the force be with you");
+
+        Post p2 = new Post(sdf.parse("28/07/2018 23:14:09"), "Good night guys", "See you tomorrow"
+                , 5);
+
+        p2.addComment(c3);
+        p2.addComment(c4);
+
+        System.out.print(p2);
+        System.out.println("---------------------------------------------------------------------");
     }
 }
