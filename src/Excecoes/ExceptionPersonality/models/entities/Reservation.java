ccf91343223151980aa497df5f3a7d2be67c89d2
CaioVKodato <caiovictorkodatot@gmail.com>
2024-08-19 16:41:47 -0300

    Criando exceptions personalizadas

--- src/Excecoes/ExceptionPersonality/models/entities/Reservation.java
+++ src/Excecoes/ExceptionPersonality/models/entities/Reservation.java
@@ -1,8 +1,10 @@
-package Excecoes.ExceptionPersonality.models;
+package Excecoes.ExceptionPersonality.models.entities;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import Excecoes.ExceptionPersonality.models.exceptions.DomainException;
 
 public class Reservation {
     private Integer roomNumber;
@@ -17,6 +19,9 @@ public class Reservation {
 
 
     public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        if (!checkout.after(checkin)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
         this.roomNumber = roomNumber;
         this.checkin = checkin;
         this.checkout = checkout;
@@ -45,13 +50,21 @@ public class Reservation {
     }
 
     public void updateDates(Date checkin, Date checkout) {
+        Date now = new Date();
+        if (checkin.before(now) || checkout.before(now)) {
+            throw new DomainException("Reservation dates for update must be future dates");
+        }
+        if (!checkout.after(checkin)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
+        
         this.checkin = checkin;
         this.checkout = checkout;
     }
 
     @Override
     public String toString() {
-        return "Room " + roomNumber + ", check-in: " +sdf.format(checkin) + ", check-out: " + sdf.format(checkout) + ", " + duration() + "nights";
+        return "Room " + roomNumber + ", check-in: " +sdf.format(checkin) + ", check-out: " + sdf.format(checkout) + ", " + duration() + " nights";
     }
 
 
---
+++ src/Excecoes/ExceptionPersonality/models/exceptions/DomainException.java
@@ -0,0 +1,10 @@
+package Excecoes.ExceptionPersonality.models.exceptions;
+
+public class DomainException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+}
