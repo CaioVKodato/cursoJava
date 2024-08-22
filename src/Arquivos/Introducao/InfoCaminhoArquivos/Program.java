c77aa29283fce974dec32d036e3373b07886c531
Caio Victor Kodato Teixeira <caiovictorkodatot@gmail.com>
2024-08-22 08:41:01 -0300

    Caminhos de arquivo

---
+++ src/Arquivos/Introducao/InfoCaminhoArquivos/Program.java
@@ -0,0 +1,20 @@
+package Arquivos.Introducao.InfoCaminhoArquivos;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+       
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Enter a file path");
+        String strPath = teclado.nextLine();
+
+        File path = new File(strPath);
+        System.out.println("getName: " + path.getName());
+        System.out.println("getParent: " + path.getParent());
+        System.out.println("getPath: " + path.getPath());
+        
+        teclado.close();
+    }
+}
