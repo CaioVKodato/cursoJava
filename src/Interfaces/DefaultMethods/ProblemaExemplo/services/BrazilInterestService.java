23e65b7491be2e35829c6261fdc2a42a0fb29a74
CaioVKodato <caiovictorkodatot@gmail.com>
2024-09-06 15:00:01 -0300

    Default Methods

---
+++ src/Interfaces/DefaultMethods/ProblemaExemplo/services/BrazilInterestService.java
@@ -0,0 +1,17 @@
+package Interfaces.DefaultMethods.ProblemaExemplo.services;
+
+
+public class BrazilInterestService implements InterestService {
+    
+    private double interestRate;
+    
+     public BrazilInterestService(double interestRate) {
+        this.interestRate = interestRate;
+     }
+    
+     @Override
+     public double getInterestRate() {
+         return interestRate;
+     }
+    }
+
