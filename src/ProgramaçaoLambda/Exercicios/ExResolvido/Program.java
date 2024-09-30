79a2ee555d270bc5bd897062185b1c00172e9849
Caio Victor Kodato Teixeira <caiovictorkodatot@gmail.com>
2024-09-30 08:08:47 -0300

    Exercicio resolvido de programacao lambda

---
+++ src/ProgramaçaoLambda/Exercicios/ExResolvido/Program.java
@@ -0,0 +1,52 @@
+package ProgramaçaoLambda.Exercicios.ExResolvido;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import ProgramaçaoLambda.ExperienciaComparable.entities.Produto;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Enter full file path: ");
+        String path = teclado.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            List<Produto> list = new ArrayList<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String fields[] = line.split(",");
+                list.add(new Produto(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            double media = list.stream()
+                            .map(p -> p.getPrice())
+                            .reduce(0.0, (x,y) -> x + y) / list.size();
+            
+            System.out.println("Media: " + String.format("%.2f", media));
+
+            Comparator<String> comp = (s1,s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+            List<String> names = list.stream()
+                                .filter(p -> p.getPrice() < media)
+                                .map(p -> p.getName())
+                                .sorted(comp.reversed())
+                                .collect(Collectors.toList());
+
+            names.forEach(System.out:: println);
+                            }
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
